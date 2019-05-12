76e9c338e195e25de74b24bb3fdb819565d8d54d
Anna Smolinska <anias05@tlen.pl>
2019-05-12 14:40:27 +0100

    zliczanie elemento ver2

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,22 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        int[] a = {4, 1, 6,4,3,1, 1, 1};
+        int wynik = 0;
+
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i] == 1){
+                wynik = wynik+1;
+            }
+
+        }
+        System.out.println(wynik);
+
+
     }
 }
